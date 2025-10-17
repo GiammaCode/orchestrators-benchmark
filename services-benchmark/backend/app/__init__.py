8a37ac5c7ef6c205ce93cb1cfa13a2a4bad8ec51
Yout Name <gianmaria.casamenti@studio.unibo.it>
2025-10-17 16:52:15 +0200

    feat: start to implement routes (app config)

---
+++ services-benchmark/backend/app/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from .extensions import mongo
+from .routes.submissions_routes import submissions_bp
+from .routes.assignments_routes import assignments_bp
+from .routes.monitoring_routes import monitoring_bp
+from .error_handlers import register_error_handlers
+from config import Config
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    #start mongo
+    mongo.init_app(app)
+
+    #Create upload directory
+    Config.UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+
+    #Create blueprints
+    app.register_blueprint(submissions_bp, url_prefix='/api')
+    app.register_blueprint(assignments_bp, url_prefix='/api')
+    app.register_blueprint(monitoring_bp, url_prefix='/api')
+
+    #start handler of error
+    register_error_handlers(app)
+
+    @app.route('/')
+    def index():
+        return{
+            "service": "Assignment Submission Service",
+            "version": "1.0.0"
+        }
+    return app
